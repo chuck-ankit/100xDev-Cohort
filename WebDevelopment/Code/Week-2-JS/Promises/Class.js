beef77bbbd59429d8d3f32316a34d26f067aab7c
Ankit kumar <ankitkumar9864@gmail.com>
2024-09-25 20:16:02 +0530

    Creation

--- WebDevelopment/Code/Week-2-JS/Promises/Class.js
+++ WebDevelopment/Code/Week-2-JS/Promises/Class.js
@@ -10,6 +10,7 @@ class Rectangle {
     }
     area() {
         const area = this.width * this.height;
+        console.log(this);
         return area;
     }
     paint() {
@@ -26,6 +27,8 @@ const rect = new Rectangle(2, 4, 'Red');
 const rect2 = new Rectangle(3,5,'Blue');
 const area = rect.area();
 const area2 = rect2.area();
+const area3 = (rect.width * rect.height);
 rect.paint();
 console.log(area);
 console.log(area2);
+console.log(area3);
