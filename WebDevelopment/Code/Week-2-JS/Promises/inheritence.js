d4f68218366cbe3959735cead2b7ce09978e0539
Ankit kumar <ankitkumar9864@gmail.com>
2024-09-25 21:10:28 +0530

    Update inheritence.js

--- WebDevelopment/Code/Week-2-JS/Promises/inheritence.js
+++ WebDevelopment/Code/Week-2-JS/Promises/inheritence.js
@@ -3,3 +3,9 @@
 const now = new Date();
 // console.log(now);
 console.log(now.getFullYear());
+
+const static = new Map();
+static.set('name', 'Ankit');
+static.set('age', 22);
+static.set('name','Raughav');
+console.log(static.get('name'),static.get('age'))
