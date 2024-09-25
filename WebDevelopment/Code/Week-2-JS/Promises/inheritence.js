77adac51fe7425837b64e11c4956542b0dff6ffb
Ankit kumar <ankitkumar9864@gmail.com>
2024-09-25 21:14:56 +0530

    Promices

--- WebDevelopment/Code/Week-2-JS/Promises/inheritence.js
+++ WebDevelopment/Code/Week-2-JS/Promises/inheritence.js
@@ -8,4 +8,62 @@ const static = new Map();
 static.set('name', 'Ankit');
 static.set('age', 22);
 static.set('name','Raughav');
-console.log(static.get('name'),static.get('age'))
+console.log(static.get('name'),static.get('age'));
+
+
+
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+
+    paint() {
+			console.log(`Painting with color ${this.color}`);
+    }
+
+    area() {
+        throw new Error('The area method must be implemented in the subclass');
+    }
+
+    getDescription() {
+        return `A shape with color ${this.color}`;
+    }
+}
+
+
+class Rectangle extends Shape {
+    constructor(width, height, color) {
+        super(color);  // Call the parent class constructor to set the color
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+
+    getDescription() {
+        return `A rectangle with width ${this.width}, height ${this.height}, and color ${this.color}`;
+    }
+}
+
+
+class Circle extends Shape {
+    constructor(radius, color) {
+        super(color);  // Call the parent class constructor to set the color
+        this.radius = radius;
+    }
+
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    getDescription() {
+        return `A circle with radius ${this.radius} and color ${this.color}`;
+    }
+}
+
+const rect = new Rectangle(4,5,'Blue');
+console.log(rect.getDescription());
+const circle = new Circle(20);
+console.log(circle.area());
