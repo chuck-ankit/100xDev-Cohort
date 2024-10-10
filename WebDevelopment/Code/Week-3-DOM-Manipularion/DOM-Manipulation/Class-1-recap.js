ee46fa3287e89dc54fcbd095078e816c563e7aea
Ankit kumar <ankitkumar9864@gmail.com>
2024-10-10 15:22:11 +0530

    Changes

--- WebDevelopment/Code/Week-3-DOM-Manipularion/DOM-Manipulation/Class-1-recap.js
+++ WebDevelopment/Code/Week-3-DOM-Manipularion/DOM-Manipulation/Class-1-recap.js
@@ -1,7 +1,32 @@
-function AddTodo(){
-    const value = document.querySelector("input").value;
-    const newTodoDivEl = document.createElement("div").value;
-    newTodoDivEl.innerHTML = value;
-    document.querySelector("body").appendChild(newTodoDivEl);
-}
+//Simple JS
 
+// function AddTodo(){
+//     const value = document.querySelector("input").value;
+//     const newTodoDivEl = document.createElement("div");
+//     newTodoDivEl.innerHTML = "<div id='new'>"+value+"</div><button onClick='DeleteBtn()' class=btn>Delete</button>";
+//     document.querySelector("#todoParent").appendChild(newTodoDivEl); 
+// }
+
+// function DeleteBtn(){
+//     document.querySelector("#todoParennt").removeChild("todoParent");
+// }
+
+
+
+// Complex Js 
+
+function AddTodo(){
+    //
+    const value = document.querySelector("input").value;
+    const spanEl = document.createElement("span");
+    const buttonEL = document.createElement("button");
+    spanEl.innerHTML = value;
+    buttonEL.innerHTML= "Delete";
+    buttonEL.className = "btn";
+    ///
+    const divEL = document.createElement("div");
+    divEL.appendChild(spanEl);
+    divEL.appendChild(buttonEL);
+    ////
+    document.querySelector("body").appendChild(divEL);
+}
