c7d32842793a5808b2d0127f9b0a4185ab17ddfe
Ankit kumar <ankitkumar9864@gmail.com>
2024-11-03 20:28:18 +0530

    week13-projec-start

--- WebDevelopment/Code/Week-13-Tailwind-CSS/week-13-tailind-class/src/pages/Screen1.jsx
+++ WebDevelopment/Code/Week-13-Tailwind-CSS/week-13-tailind-class/src/pages/Screen1.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import './Screen1.css';
+import { Link } from 'react-router-dom';
+import './Screen.css';
 
 function Screen1() {
-  return <h2 className="text-center mt-8 text-2xl">This is Screen 1</h2>;
+  return (
+    <div className="screen1-container">
+      <div className='title-logo text-center items-start mt-[70px]'>
+        <div><image src='../assets/webinar.png'></image></div>
+        <div className='text-bright-green-400 text-2xl'>Webinar</div>
+        <div className='text-white text-2xl'>.gg</div>
+      </div>
+
+
+``
+    </div>
+  );
 }
 
 export default Screen1;
